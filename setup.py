59f5618c1f40ba21063d345e582fb8665ea2f0cd
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-02-17 15:48:51 -0800

    Cambia a versión CLI

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
         "console_scripts": [
             "crea_tamagno_poblacion_gatos = gatos.generaTamanioPoblacionGatos:cli",
             "crea_probabilidad_atrapar_gatos = gatos.generaProbabilidadAtraparGatosRemanentes:cli",
-            "crea_valores_reporte = gatos.generaValoresReporte:cli"
+            "crea_valores_reporte = gatos.generaValoresReporte:cli",
+            "crea_tabla_pvalor = gatos.tabla_p_valor_erradicacion_gatos:cli"
         ]
     },
     install_requires = [
