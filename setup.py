085f012070cc1ef93056fd84ab35f59236d01063
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-02-17 17:12:18 -0800

    Actualiza la versión de la CLI

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="gatos",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages() ,
     include_package_data=True,
     entry_points={
         "console_scripts": [
             "crea_tamagno_poblacion_gatos = gatos.generaTamanioPoblacionGatos:cli",
-            "crea_probabilidad_atrapar_gatos = gatos.generaProbabilidadAtraparGatosRemanentes:cli",
-            "crea_valores_reporte = gatos.generaValoresReporte:cli",
             "crea_tabla_pvalor = gatos.tabla_p_valor_erradicacion_gatos:cli"
         ]
     },
