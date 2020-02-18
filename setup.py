af5479e6896064b80aa96d5e9136670cb34609d1
Genaro Velazquez <genaro.velazquez@islas.org.mx>
2020-02-17 17:28:51 -0800

    Cambia diferencias de código en la instalación

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="gatos",
     version="0.2.0",
-    packages=find_packages() ,
+    packages=find_packages(),
     include_package_data=True,
+    install_requires = [
+        "Click",
+    ],
     entry_points={
         "console_scripts": [
             "crea_tamagno_poblacion_gatos = gatos.generaTamanioPoblacionGatos:cli",
             "crea_tabla_pvalor = gatos.tabla_p_valor_erradicacion_gatos:cli"
         ]
-    },
-    install_requires = [
-        "Click"
-    ]
+    }
 )
