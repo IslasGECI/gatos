725797845bf7ac21ceab06a2b4795b9ad7326eef
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-21 17:48:54 -0700

    🔥 Write dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="gatos",
-    version="0.2.1",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires = [
         "Click",
+        "metadata_tools @ git+https://git@github.com/IslasGECI/metadata_tools@v0.2.2",
     ],
     entry_points={
         "console_scripts": [
