97588b33c18bbbb3874a21a986e6c095b12a88fe
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-21 21:27:05 -0700

    ⚗️ Try resolve its dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     include_package_data=True,
     install_requires = [
         "Click",
+        "metadatatools @ git+https://github.com/IslasGECI/metadata_tools@v0.2.2",
     ],
     entry_points={
         "console_scripts": [
