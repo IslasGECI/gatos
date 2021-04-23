3e67d55027435f0d76950be76ac87b981e7eb96c
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-22 20:22:23 -0700

    🎨 Make format with black

--- gatos/generaTamanioPoblacionGatos.py
+++ gatos/generaTamanioPoblacionGatos.py
@@ -19,7 +19,7 @@ def cli():
 @cli.command(short_help="Cálcula la distribución posterior para el tamaño de la población inicial")
 @click.option("--resource", "-r", type=click.Path(), help="Nombre del recurso csv")
 @click.option("--output-file", "-o", type=click.Path(), help="Nombre del archivo de salida csv")
-@click.option("--iterations", "-i", default = 1_000_000, type=int, help="Número de iteraciones")
+@click.option("--iterations", "-i", default=1_000_000, type=int, help="Número de iteraciones")
 def calculate(**argumentos):
     DatosSocorro = metadatatools.import_tabular_data_resource(argumentos["resource"])
     nombre_esfuerzo: str = "Esfuerzo"
