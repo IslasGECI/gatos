5ca4bb2271fded1b3f98b4ecdcba3f0b8157f31e
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-23 12:22:28 -0700

    🎨 Follow flake8

--- tests/test_tabla_p_valor_erradicacion_gatos.py
+++ tests/test_tabla_p_valor_erradicacion_gatos.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 def test_nothing():
-    bash_command = f"python gatos/tabla_p_valor_erradicacion_gatos.py --help"
+    bash_command = "python gatos/tabla_p_valor_erradicacion_gatos.py --help"
     subprocess.check_call(bash_command, shell=True)
