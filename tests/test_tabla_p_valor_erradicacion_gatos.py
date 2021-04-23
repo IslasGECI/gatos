0b460671fefc069859769154748f12bcafff42cf
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-23 11:52:01 -0700

    🏹👾✅ Hunt mutant in CLI

--- tests/test_tabla_p_valor_erradicacion_gatos.py
+++ tests/test_tabla_p_valor_erradicacion_gatos.py
@@ -1,4 +1,5 @@
-import gatos as rms
+import subprocess
 
 def test_nothing():
-    pass
+    bash_command = f"python gatos/tabla_p_valor_erradicacion_gatos.py --help"
+    subprocess.check_call(bash_command, shell=True)
