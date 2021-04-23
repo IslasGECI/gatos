3f16c41a1cc880c3e69f547ef852c09113deeba3
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-23 12:18:43 -0700

    ✅ Add tests to builder

---
+++ tests/test_PopulationEstimator.py
@@ -0,0 +1,10 @@
+from gatos.PopulationEstimator import PopulationEstimator
+
+def test_init():
+    catch = [1,2]
+    effort = [100, 200]
+    file_name = "nombre_archivo"
+    Estimator = PopulationEstimator(effort, catch, file_name)
+    assert Estimator.esfuerzo == effort
+    assert Estimator.capturas == catch
+    assert Estimator._nombre_archivo == file_name
