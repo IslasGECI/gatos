05e6d7a9fdb3ae942fd466bf917eb955d4eea364
Xavier Paredes <xavier.paredes@islas.org.mx>
2021-04-23 13:05:29 -0700

    ✅ add a new test

--- tests/test_calculator_p_value.py
+++ tests/test_calculator_p_value.py
@@ -1,8 +1,15 @@
 from gatos.calculator_p_value import CalculatorPValue
+import pandas as pd
+
+total_capturas = 15
+archivo = "tests/data/example.csv"
+
+
+def test_read_posterior():
+    pass
 
 
 def test_set_total_capturas():
-    total_capturas = 10
     calculador = CalculatorPValue()
     calculador.set_total_capturas(total_capturas)
     assert calculador.capturas == total_capturas
