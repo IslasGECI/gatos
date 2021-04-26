0aa6aeb4041b0c76c12ce1028490e0dd1006a534
Xavier Paredes <xavier.paredes@islas.org.mx>
2021-04-26 14:22:01 -0700

    ✅ add a new test

--- tests/test_calculator_p_value.py
+++ tests/test_calculator_p_value.py
@@ -1,11 +1,20 @@
 from gatos.calculator_p_value import CalculatorPValue
+from pandas.util.testing import assert_frame_equal
+import pandas as pd
 
-total_capturas = 15
+
+total_capturas = 2
 archivo = "tests/data/example.csv"
 
 
 def test_read_posterior():
-    pass
+    calculador = CalculatorPValue()
+    calculador.set_total_capturas(total_capturas)
+    calculador.read_posterior(archivo)
+    expected_datos = pd.read_csv(archivo)
+    assert_frame_equal(expected_datos, calculador.datos)
+    expected_remanented_cats = expected_datos.No + total_capturas
+    assert expected_remanented_cats[0] != calculador.remanented_cats[0]
 
 
 def test_set_total_capturas():
