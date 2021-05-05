c3f6af8c20446624c180cd5560f1a61ad34b2441
Fernando <fernando.alvarez@islas.org.mx>
2021-05-05 10:12:55 -0700

    ⚡️ Update python version

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.2.2",
     packages=find_packages(),
     include_package_data=True,
+    python_requires='>=3.8',
     install_requires = [
         "Click",
         "metadatatools @ git+https://git@github.com/IslasGECI/metadata_tools@v0.2.2",
