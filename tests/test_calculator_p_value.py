c4ba29903fe5fc723d3affe17fa8de4fe54423c4
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-28 11:14:00 -0700

    ♻️ Make a class test
    Stargirl suggest this

--- tests/test_calculator_p_value.py
+++ tests/test_calculator_p_value.py
@@ -4,7 +4,7 @@ import pandas as pd  # type: ignore
 import numpy as np  # type: ignore
 
 
-total_capturas = 15
+total_capturas = 1
 archivo = "tests/data/example.csv"
 
 calculador = CalculatorPValue()
@@ -13,45 +13,46 @@ calculador.read_posterior(archivo)
 calculador.calculate_range_remanented_cats()
 calculador.calculate_high_probability()
 calculador.calculate_remanented_cat_more_probably()
+calculador.probability()
+
+class Test_CalculatorPValue:
+    def test_read_posterior(self):
+        expected_datos = pd.read_csv(archivo)
+        assert_frame_equal(expected_datos, calculador.datos)
+        expected_remanented_cats = expected_datos.No + total_capturas
+        assert expected_remanented_cats[0] != calculador.remanented_cats[0]
 
 
-def test_read_posterior():
-    expected_datos = pd.read_csv(archivo)
-    assert_frame_equal(expected_datos, calculador.datos)
-    expected_remanented_cats = expected_datos.No + total_capturas
-    assert expected_remanented_cats[0] != calculador.remanented_cats[0]
+    def test_set_total_capturas(self):
+        assert calculador.capturas == total_capturas
 
 
-def test_set_total_capturas():
-    assert calculador.capturas == total_capturas
+    def test_calculate_range_remanented_cats(self):
+        expected_n_bins = max(calculador.datos.No.unique()) - min(calculador.datos.No.unique())
+        expected_hist, expected_bins = np.histogram(calculador.remanented_cats, bins=expected_n_bins)
+
+        np.testing.assert_array_equal(calculador.bins, expected_bins)
+        np.testing.assert_array_equal(calculador.hist, expected_hist)
 
 
-def test_calculate_range_remanented_cats():
-    expected_n_bins = max(calculador.datos.No.unique()) - min(calculador.datos.No.unique())
-    expected_hist, expected_bins = np.histogram(calculador.remanented_cats, bins=expected_n_bins)
+    def test_calculate_high_probability(self):
+        expected_n_bins = max(calculador.datos.No.unique()) - min(calculador.datos.No.unique())
+        expected_hist, expected_bins = np.histogram(calculador.remanented_cats, bins=expected_n_bins)
 
-    np.testing.assert_array_equal(calculador.bins, expected_bins)
-    np.testing.assert_array_equal(calculador.hist, expected_hist)
+        maximo = max(expected_hist)
+        indice_mas_probable = np.where(expected_hist == maximo)[0]
 
-
-def test_calculate_high_probability():
-    expected_n_bins = max(calculador.datos.No.unique()) - min(calculador.datos.No.unique())
-    expected_hist, expected_bins = np.histogram(calculador.remanented_cats, bins=expected_n_bins)
-
-    maximo = max(expected_hist)
-    indice_mas_probable = np.where(expected_hist == maximo)[0]
-
-    assert calculador.maximo == maximo
-    assert calculador.indice_mas_probable[0] == indice_mas_probable[0]
+        assert calculador.maximo == maximo
+        assert calculador.indice_mas_probable[0] == indice_mas_probable[0]
 
 
-def test_calculate_remanented_cat_more_probably():
-    expected_n_bins = max(calculador.datos.No.unique()) - min(calculador.datos.No.unique())
-    expected_hist, expected_bins = np.histogram(calculador.remanented_cats, bins=expected_n_bins)
+    def test_calculate_remanented_cat_more_probably(self):
+        expected_n_bins = max(calculador.datos.No.unique()) - min(calculador.datos.No.unique())
+        expected_hist, expected_bins = np.histogram(calculador.remanented_cats, bins=expected_n_bins)
 
-    maximo = max(expected_hist)
-    indice_mas_probable = np.where(expected_hist == maximo)[0]
+        maximo = max(expected_hist)
+        indice_mas_probable = np.where(expected_hist == maximo)[0]
 
-    remanented_cat_more_probably = expected_bins[indice_mas_probable][0]
+        remanented_cat_more_probably = expected_bins[indice_mas_probable][0]
 
-    assert calculador.remanented_cat_more_probably == remanented_cat_more_probably
+        assert calculador.remanented_cat_more_probably == remanented_cat_more_probably
