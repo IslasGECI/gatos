504c9203392d70e4b9185fcadbe48b6aec40b809
Fernando <fernando.alvarez@islas.org.mx>
2021-10-14 09:03:51 -0700

    ✨Update posterior sampling command to new version

--- gatos/PopulationEstimator.py
+++ gatos/PopulationEstimator.py
@@ -4,6 +4,7 @@ import pymc3 as pm3
 import arviz as az
 from geci_plots import geci_plot, roundup
 
+RANDOM_SEED = 22
 
 class PopulationEstimator:
     """Clase encargada de encontrar el tamaño inicial de la población utilizando
@@ -116,7 +117,7 @@ class PopulationEstimator:
         df_loo.keys(['waic', 'waic_se', 'p_waic']).to_json(self.json_output_path + "waic_results.json")
 
     def sample_predictive_posterior(self):
-        self.ppc = pm3.sample_ppc(self.trace, model=self.cats_model, samples=100)
+        self.ppc = pm3.sample_posterior_predictive(self.trace, model=self.cats_model, samples=100, random_seed=RANDOM_SEED)
 
     def plot_data_and_predictive_points(self, plot_name="predictive_posterior.png"):
         fig , ax = geci_plot()
