0409362b91e8c207a3fac8b91324dc2c8bed24eb
Xavier Paredes <jxavierpp1@gmail.com>
2021-10-21 18:09:09 +0000

    🔖 Update version tag to v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gatos",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=3.8",
