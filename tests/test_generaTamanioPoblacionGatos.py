ba49aa75ce162e6eac01dcb3e185f405cb5c89d9
Xavier Paredes <jxavierpp1@gmail.com>
2022-01-20 23:53:16 +0000

    🧪 Add a failing test.

--- tests/test_generaTamanioPoblacionGatos.py
+++ tests/test_generaTamanioPoblacionGatos.py
@@ -1,3 +1,6 @@
+import metadatatools
+from pandas.util.testing import assert_frame_equal
+from gatos.generaTamanioPoblacionGatos import *
 import subprocess
 import re
 
@@ -14,3 +17,12 @@ def test_crea_tamagno_poblacion_gatos_help():
 def test_crea_tamagno_poblacion_gatos_calculate_help():
     bash_command = "crea_tamagno_poblacion_gatos calculate --help"
     subprocess.check_call(bash_command, shell=True)
+
+
+def test_get_effort():
+    datosSocorro = metadatatools.import_tabular_data_resource(
+        "tests/data/esfuerzos_capturas_gatos_socorro/esfuerzo_capturas_mensuales_gatos_socorro.csv"
+    )
+    obtained_effort = get_effort("Esfuerzo", datosSocorro)
+    expected_effort = 25725
+    assert_frame_equal(expected_effort, obtained_effort)
