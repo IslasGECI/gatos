e45df20ba1428ea07bb0368f960bd02f64de78f5
Mario Villasante <mario.villasante@islas.org.mx>
2023-03-06 13:20:00 -0800

    🆙 Update mid version

--- gatos/__init__.py
+++ gatos/__init__.py
@@ -1,7 +1,7 @@
 """Gatos package"""
 
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 # from .DataAndMetadataCats import *
 from .cpue_time_serie import *  # noqa
 from .get_capture_and_effort_by_zone import *  # noqa
