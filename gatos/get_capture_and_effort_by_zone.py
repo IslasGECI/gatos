c62e3edc4a0cdb9f9ac20c6191611d5e72dc9536
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-03-06 17:04:41 -0800

    🔴🚧 Add `get_yearly_capture_and_effort_by_zone()`

--- gatos/get_capture_and_effort_by_zone.py
+++ gatos/get_capture_and_effort_by_zone.py
@@ -1,10 +1,54 @@
 import pandas as pd
+import typer
+import warnings
+
+app = typer.Typer()
+
+
+@app.command()
+def write_effort_and_captures_by_zone(
+    input_path: str = typer.Option("", help="Input file path"),
+    output_path: str = typer.Option("", help="Output file path"),
+):
+    weekly_effort_and_capture = pd.read_csv(input_path)
+    grouped_data = get_monthly_capture_and_effort_by_zone(weekly_effort_and_capture)
+    grouped_data.to_csv(output_path, index=False)
 
 
 def get_capture_and_effort_by_zone(weekly_effort_and_captures_data: pd.DataFrame):
-    weekly_effort_and_captures_data.Fecha = weekly_effort_and_captures_data.Fecha.apply(
-        lambda x: x[0:7]
+    warnings.warn(
+        "Use get_monthly_capture_and_effort_by_zone() instead of get_capture_and_effort_by_zone()",
+        DeprecationWarning,
     )
+    return get_monthly_capture_and_effort_by_zone(weekly_effort_and_captures_data)
+
+
+def get_monthly_capture_and_effort_by_zone(weekly_effort_and_captures_data: pd.DataFrame):
+    return get_year_and_month_from_date(weekly_effort_and_captures_data)
+
+
+def get_yearly_capture_and_effort_by_zone(weekly_effort_and_captures_data: pd.DataFrame):
+    return get_year_from_date(weekly_effort_and_captures_data)
+
+
+def get_year_and_month_from_date(weekly_effort_and_captures_data):
+    string_length = 7
+    return cut_date_string(weekly_effort_and_captures_data, string_length)
+
+
+def get_year_from_date(weekly_effort_and_captures_data):
+    string_length = 4
+    return cut_date_string(weekly_effort_and_captures_data, string_length)
+
+
+def cut_date_string(weekly_effort_and_captures_data, string_length):
+    weekly_effort_and_captures_data.Fecha = weekly_effort_and_captures_data.Fecha.apply(
+        lambda x: x[0:string_length]
+    )
+    return sum_captures_and_effort_by_date_and_zone(weekly_effort_and_captures_data)
+
+
+def sum_captures_and_effort_by_date_and_zone(weekly_effort_and_captures_data):
     new_names = {"Fecha": "Date", "Zona": "Zone", "Esfuerzo": "Effort", "Capturas": "Captures"}
     weekly_effort_and_captures_data.rename(columns=new_names, inplace=True)
     capture_and_effort = weekly_effort_and_captures_data.groupby(["Date", "Zone"]).sum()
--- tests/test_monthly_effort_and_captures_by_zone.py
+++ tests/test_monthly_effort_and_captures_by_zone.py
@@ -1,12 +1,18 @@
-from gatos import get_capture_and_effort_by_zone
+from gatos import (
+    get_capture_and_effort_by_zone,
+    write_effort_and_captures_by_zone,
+    get_yearly_capture_and_effort_by_zone,
+)
 
 import pandas as pd
+import os
+
+
+input_path = "tests/data/esfuerzo_capturas_semanales_iso8601.csv"
 
 
 def test_get_capture_and_effort_by_zone():
-    weekly_effort_and_captures_data = pd.read_csv(
-        "tests/data/esfuerzo_capturas_semanales_iso8601.csv"
-    )
+    weekly_effort_and_captures_data = pd.read_csv(input_path)
     obtained = get_capture_and_effort_by_zone(weekly_effort_and_captures_data)
     expected_columns = ["Date", "Zone", "Effort", "Captures"]
     obtained_columns = obtained.columns
@@ -19,3 +25,27 @@ def test_get_capture_and_effort_by_zone():
     expected_length = 15
     obtained_length = len(obtained)
     assert obtained_length == expected_length
+
+
+def test_get_yearly_capture_and_effort_by_zone():
+    weekly_effort_and_captures_data = pd.read_csv(input_path)
+    obtained = get_yearly_capture_and_effort_by_zone(weekly_effort_and_captures_data)
+    expected_columns = ["Date", "Zone", "Effort", "Captures"]
+    obtained_columns = obtained.columns
+    assert (obtained_columns == expected_columns).all()
+
+    expected_dates = ["2023"]
+    obtained_dates = obtained.Date.unique()
+    assert (expected_dates == obtained_dates).all()
+
+    expected_length = 8
+    obtained_length = len(obtained)
+    assert obtained_length == expected_length
+
+    assert False
+
+
+def test_write_effort_and_captures_by_zone():
+    output_path = "tests/data/effort_and_captures_by_zone.csv"
+    write_effort_and_captures_by_zone(input_path, output_path)
+    assert os.path.exists(output_path)
