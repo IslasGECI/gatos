77f440b97f8785f9fff0aef89bf6ceb20e01085e
Mario Villasante <mario.villasante@islas.org.mx>
2023-04-12 16:42:37 -0700

    💬 Add datapackage click option

--- gatos/generaTamanioPoblacionGatos.py
+++ gatos/generaTamanioPoblacionGatos.py
@@ -21,26 +21,21 @@ def cli():
 @click.option("--resource", "-r", type=click.Path(), help="Nombre del recurso csv")
 @click.option("--output-file", "-o", type=click.Path(), help="Nombre del archivo de salida csv")
 @click.option("--iterations", "-i", default=1_000_000, type=int, help="Número de iteraciones")
+@click.option("--datapackage", "-d", default=True, type=bool, help="Read data from datapackage")
 def calculate(**argumentos):
     run_population_estimator(argumentos)
 
 
 def run_population_estimator(argumentos):
+    trapps_per_person = 30
+    number_of_trappers = 7
+    working_days = 5
+    man_days = trapps_per_person * number_of_trappers * working_days
     if argumentos["datapackage"]:
-        DatosSocorro = metadatatools.import_tabular_data_resource(argumentos["resource"])
-        nombre_esfuerzo: str = "Esfuerzo"
-        nombre_capturas: str = "Capturas"
-        esfuerzo: np.array = np.array(
-            DatosSocorro.get_value(nombre_esfuerzo) / (30 * 7 * 5)
-        )  # Días hombre: 30 trampas, 7 tramperos, 5 días
-        capturas: np.array = np.array(DatosSocorro.get_value(nombre_capturas))
+        esfuerzo, capturas = get_effort_and_captures_with_datapackage(argumentos, man_days)
     else:
-        monthly_summary = pd.read_csv(argumentos["resource"])
-        esfuerzo = monthly_summary.Esfuerzo / (30 * 7 * 5)
-        capturas = monthly_summary.Capturas
+        esfuerzo, capturas = get_effort_and_captures_without_datapackage(argumentos, man_days)
 
-    print("Esfuerzo")
-    print(esfuerzo)
     # Se busca el tamaño de la población
     iteraciones = argumentos["iterations"]
     estimador_poblacion_inicial: PopulationEstimator = PopulationEstimator(
@@ -51,3 +46,19 @@ def run_population_estimator(argumentos):
         n_datos_descartados=int(np.ceil(iteraciones * 0.1)),
     )
     estimador_poblacion_inicial.run_model_diagnostics()
+
+
+def get_effort_and_captures_without_datapackage(argumentos, man_days):
+    monthly_summary = pd.read_csv(argumentos["resource"])
+    esfuerzo = monthly_summary.Esfuerzo.to_numpy() / man_days
+    capturas = monthly_summary.Capturas.to_numpy()
+    return esfuerzo, capturas
+
+
+def get_effort_and_captures_with_datapackage(argumentos, man_days):
+    DatosSocorro = metadatatools.import_tabular_data_resource(argumentos["resource"])
+    nombre_esfuerzo: str = "Esfuerzo"
+    nombre_capturas: str = "Capturas"
+    esfuerzo: np.array = np.array(DatosSocorro.get_value(nombre_esfuerzo) / man_days)
+    capturas: np.array = np.array(DatosSocorro.get_value(nombre_capturas))
+    return esfuerzo, capturas
--- tests/test_generaTamanioPoblacionGatos.py
+++ tests/test_generaTamanioPoblacionGatos.py
@@ -8,7 +8,7 @@ import re
 def tests_run_population_estimatior():
     argumentos = {
         "resource": "tests/data/monthly_effort_and_captures.csv",
-        "iterations": 2,
+        "iterations": 100,
         "output_file": "tests/data/distribucion_posterior.csv",
         "datapackage": True,
     }
@@ -19,44 +19,36 @@ def tests_run_population_estimatior():
     assert obtained_leo_hash == expected_leo_hash
     predictive_figure = "reports/figures/predictive_posterior.png"
     obtained_predictive_hash = hashlib.md5(open(predictive_figure, "rb").read()).hexdigest()
-    expected_predictive_hash = "d6013174542c7e938614dab98dacd23b"
+    expected_predictive_hash = "d17e1fba7eba6f76ea75c88f3c83daa4"
     assert obtained_predictive_hash == expected_predictive_hash
     posterior_path = "tests/data/distribucion_posterior.csv"
     obtained_posterior_hash = hashlib.md5(open(posterior_path, "rb").read()).hexdigest()
-    expected_posterior_hash = "5a33180e99bda7417b4e40d9330776bb"
+    expected_posterior_hash = "2de5ff7c555a175d632697383313b913"
     assert obtained_posterior_hash == expected_posterior_hash
     loo_path = "reports/non-tabular/loo_results.json"
     obtained_loo_hash = hashlib.md5(open(loo_path, "rb").read()).hexdigest()
-    expected_loo_hash = "93c387c99bd41bb7c4cb9288d7a46e9c"
+    expected_loo_hash = "88b5f9b02123a9466803b1e2531aaded"
     assert obtained_loo_hash == expected_loo_hash
     waic_path = "reports/non-tabular/waic_results.json"
     obtained_waic_hash = hashlib.md5(open(waic_path, "rb").read()).hexdigest()
-    expected_waic_hash = "c132351ac68fda1aca6b4354145de8bb"
+    expected_waic_hash = "5aeb3923ef67ccce2fa6daab5028fb8d"
     assert obtained_waic_hash == expected_waic_hash
 
     argumentos = {
         "resource": "tests/data/helpers/monthly_effort_and_captures.csv",
-        "iterations": 2,
+        "iterations": 100,
         "output_file": "tests/data/distribucion_posterior_without_datapackage.csv",
         "datapackage": False,
     }
     run_population_estimator(argumentos)
-    assert False
-    predictive_figure = "reports/figures/predictive_posterior.png"
     obtained_predictive_hash = hashlib.md5(open(predictive_figure, "rb").read()).hexdigest()
-    expected_predictive_hash = "967d7b046bdc6f969d113be3e91a7d66"
     assert obtained_predictive_hash == expected_predictive_hash
     posterior_path = "tests/data/distribucion_posterior_without_datapackage.csv"
     obtained_posterior_hash = hashlib.md5(open(posterior_path, "rb").read()).hexdigest()
-    expected_posterior_hash = "5a33180e99bda7417b4e40d9330776bb"
     assert obtained_posterior_hash == expected_posterior_hash
-    loo_path = "reports/non-tabular/loo_results.json"
     obtained_loo_hash = hashlib.md5(open(loo_path, "rb").read()).hexdigest()
-    expected_loo_hash = "87cbf7ed36665dbad50b0345261373c4"
     assert obtained_loo_hash == expected_loo_hash
-    waic_path = "reports/non-tabular/waic_results.json"
     obtained_waic_hash = hashlib.md5(open(waic_path, "rb").read()).hexdigest()
-    expected_waic_hash = "e45b2d2989895acd81f386c7b36264f3"
     assert obtained_waic_hash == expected_waic_hash
 
 
@@ -72,3 +64,7 @@ def test_crea_tamagno_poblacion_gatos_help():
 def test_crea_tamagno_poblacion_gatos_calculate_help():
     bash_command = "crea_tamagno_poblacion_gatos calculate --help"
     subprocess.check_call(bash_command, shell=True)
+    obtained_stdout = subprocess.getoutput(bash_command)
+    expected = "--datapackage"
+    is_there = re.search(expected, obtained_stdout)
+    assert is_there
