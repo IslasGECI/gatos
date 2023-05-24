a23db3fec4992fbb5f94aa3697b43cee7e2ce377
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-24 20:15:12 +0000

    ♻️  Refactor

--- gatos/get_capture_and_effort_by_zone.py
+++ gatos/get_capture_and_effort_by_zone.py
@@ -47,11 +47,11 @@ def get_monthly_capture_and_effort_by_zone(weekly_effort_and_captures_data):
 
 
 def get_cummulative_effort_and_captures_by_year(weekly_effort_and_captures_data):
-    yearly_capture_and_effor_by_zone = get_yearly_capture_and_effort_by_zone(
+    yearly_capture_and_effort = get_yearly_capture_and_effort_by_zone(
         weekly_effort_and_captures_data
-    )
+    ).drop(columns=["Zone"])
     year_capture_and_effort = (
-        yearly_capture_and_effor_by_zone.groupby(["Date"]).sum(numeric_only=True).cumsum()
+        yearly_capture_and_effort.groupby(["Date"]).sum(numeric_only=True).cumsum()
     )
     return year_capture_and_effort.reset_index()
 
