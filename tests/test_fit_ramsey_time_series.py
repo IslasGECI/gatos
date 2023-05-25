735441a539bccb0f47d701d0a042b17b36368e75
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-05-25 16:54:48 -0700

    🛑🧪 Test 'set_up_ramsey_time_series()': columns name

--- tests/test_fit_ramsey_time_series.py
+++ tests/test_fit_ramsey_time_series.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from gatos import set_up_ramsey_time_series
+
 
 data = pd.DataFrame({"Effort": [1, 2, 3, 4, 5, 6], "Captures": [1, 1, 1, 1, 1, 1]})
 
@@ -8,3 +10,4 @@ def test_set_up_ramsey_time_series():
         {"CPUE": [1, 1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6], "Captures": [1, 2, 3, 4, 5, 6]}
     )
     obtained = set_up_ramsey_time_series(data)
+    assert obtained.columns() == ["CPUE", "Captures"]
