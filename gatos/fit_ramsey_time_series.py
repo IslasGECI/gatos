5e7c1f507acdbe9265b9b3895e9b475b60161771
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-26 18:57:03 +0000

    ♻️  Refactor

--- gatos/fit_ramsey_time_series.py
+++ gatos/fit_ramsey_time_series.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 def set_up_ramsey_time_series(data):
-    data["Captures"] = data["Captures"].cumsum()
+    data["Cumulative_captures"] = data["Captures"].cumsum()
     data["CPUE"] = 0
-    return data[["CPUE", "Captures"]]
+    return data[["CPUE", "Cumulative_captures"]]
--- tests/test_fit_ramsey_time_series.py
+++ tests/test_fit_ramsey_time_series.py
@@ -7,16 +7,19 @@ data = pd.DataFrame({"Effort": [1, 2, 3, 4, 5, 6], "Captures": [1, 1, 1, 1, 1, 1
 
 def test_set_up_ramsey_time_series():
     expected = pd.DataFrame(
-        {"CPUE": [1, 1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6], "Captures": [1, 2, 3, 4, 5, 6]}
+        {"CPUE": [1, 1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6], "Cumulative_captures": [1, 2, 3, 4, 5, 6]}
     )
     obtained = set_up_ramsey_time_series(data)
-    assert (obtained.columns == ["CPUE", "Captures"]).all()
-    assert (obtained.Captures == expected.Captures).all()
+    assert (obtained.columns == ["CPUE", "Cumulative_captures"]).all()
+    assert (obtained.Cumulative_captures == expected.Cumulative_captures).all()
 
     data_2 = pd.DataFrame({"Effort": [2, 2, 2, 2, 2, 2], "Captures": [1, 2, 1, 1, 2, 1]})
     obtained = set_up_ramsey_time_series(data_2)
 
     expected = pd.DataFrame(
-        {"CPUE": [1 / 2, 2 / 2, 1 / 2, 1 / 2, 2 / 2, 1 / 2], "Captures": [1, 3, 4, 5, 7, 8]}
+        {
+            "CPUE": [1 / 2, 2 / 2, 1 / 2, 1 / 2, 2 / 2, 1 / 2],
+            "Cumulative_captures": [1, 3, 4, 5, 7, 8],
+        }
     )
-    assert (obtained.Captures == expected.Captures).all()
+    assert (obtained.Cumulative_captures == expected.Cumulative_captures).all()
