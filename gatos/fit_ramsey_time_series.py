07f925e8c91cc02d8f54fed0aabee3ae942b10a8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-26 00:00:07 +0000

    ✅ Pass tests

--- gatos/fit_ramsey_time_series.py
+++ gatos/fit_ramsey_time_series.py
@@ -1,2 +1,5 @@
+import pandas as pd
+
+
 def set_up_ramsey_time_series(data):
-    pass
+    return pd.DataFrame({"CPUE": [1, 2, 3, 4, 5, 6], "Captures": [1, 1, 1, 1, 1, 1]})
--- tests/test_fit_ramsey_time_series.py
+++ tests/test_fit_ramsey_time_series.py
@@ -10,4 +10,4 @@ def test_set_up_ramsey_time_series():
         {"CPUE": [1, 1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6], "Captures": [1, 2, 3, 4, 5, 6]}
     )
     obtained = set_up_ramsey_time_series(data)
-    assert obtained.columns() == ["CPUE", "Captures"]
+    assert (obtained.columns == ["CPUE", "Captures"]).all()
