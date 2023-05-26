b0bf80788b643f5334d62f1a7ca3906b2e44931b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-26 20:01:56 +0000

    ✅ Pass tests

--- gatos/fit_ramsey_time_series.py
+++ gatos/fit_ramsey_time_series.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -5,3 +6,7 @@ def set_up_ramsey_time_series(data):
     data["Cumulative_captures"] = data["Captures"].cumsum()
     data["CPUE"] = data["Captures"] / data["Effort"]
     return data[["CPUE", "Cumulative_captures"]]
+
+
+def fit_ramsey_plot(data):
+    pass
