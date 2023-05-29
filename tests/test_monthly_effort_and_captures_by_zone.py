f35c6e122e3e4ff57d1d1755aad335483e522aef
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-29 23:35:16 +0000

    🛑🧪 Fail tests

--- tests/test_monthly_effort_and_captures_by_zone.py
+++ tests/test_monthly_effort_and_captures_by_zone.py
@@ -6,6 +6,7 @@ from gatos import (
     get_yearly_capture_and_effort_by_zone,
     select_effort_and_captures_by_year,
     write_effort_and_captures_by_zone_for_year,
+    write_effort_and_capture_with_slopes,
     write_yearly_cummulative_effort_and_captures,
     years_from_data,
 )
@@ -81,6 +82,18 @@ def test_write_effort_and_captures_by_zone():
     year = "2022"
     write_effort_and_captures_by_zone_for_year(input_path, output_path, year)
     assert os.path.exists(output_path)
+
+
+def test_write_effort_and_capture_with_slopes():
+    output_path = "tests/data/slope_time_series.csv"
+    monthly_path = "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    write_effort_and_capture_with_slopes(monthly_path, output_path)
+    assert os.path.exists(output_path)
+    expected_hash = "9a4aaa1cf8d7d30c64521ca2fe727b7b"
+    obtained_hash = hashlib.md5(open(output_path, "rb").read()).hexdigest()
+    assert obtained_hash == expected_hash, "Hash of csv with slopes"
 
 
 def test_write_yearly_cummulative_effort_and_captures():
