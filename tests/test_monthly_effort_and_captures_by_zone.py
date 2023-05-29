8f1819a513a1b9c224b93216846999242f7764ef
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-29 23:36:14 +0000

    🛑🧪 Fail tests

--- tests/test_monthly_effort_and_captures_by_zone.py
+++ tests/test_monthly_effort_and_captures_by_zone.py
@@ -6,7 +6,7 @@ from gatos import (
     get_yearly_capture_and_effort_by_zone,
     select_effort_and_captures_by_year,
     write_effort_and_captures_by_zone_for_year,
-    write_effort_and_capture_with_slopes,
+    write_effort_and_captures_with_slopes,
     write_yearly_cummulative_effort_and_captures,
     years_from_data,
 )
@@ -89,7 +89,7 @@ def test_write_effort_and_capture_with_slopes():
     monthly_path = "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
     if os.path.exists(output_path):
         os.remove(output_path)
-    write_effort_and_capture_with_slopes(monthly_path, output_path)
+    write_effort_and_captures_with_slopes(monthly_path, output_path)
     assert os.path.exists(output_path)
     expected_hash = "9a4aaa1cf8d7d30c64521ca2fe727b7b"
     obtained_hash = hashlib.md5(open(output_path, "rb").read()).hexdigest()
