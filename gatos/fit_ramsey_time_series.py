3df092e8d7f4233f1ae6d0b3f717f7ebfbdae25e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-29 20:41:42 +0000

    ✅ Pass tests

--- gatos/fit_ramsey_time_series.py
+++ gatos/fit_ramsey_time_series.py
@@ -3,6 +3,8 @@ import numpy as np
 
 def add_slopes_to_effort_capture_data(data):
     data["slope"] = np.nan
+    slope_status = get_status_slopes(data)
+    data["slope"].iloc[5:] = slope_status
     return data
 
 
