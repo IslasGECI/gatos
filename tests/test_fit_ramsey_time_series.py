132168c96ae520ea27fd0477d73f825cf8881e46
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-05-31 18:53:17 +0000

    🛑🧪 Fail tests

--- tests/test_fit_ramsey_time_series.py
+++ tests/test_fit_ramsey_time_series.py
@@ -105,7 +105,7 @@ def test_set_up_ramsey_time_series():
     )
     assert (obtained.Cumulative_captures == expected.Cumulative_captures).all()
     singular_data = pd.DataFrame({"Esfuerzo": [2, 2, 2, 2, 2, 2], "Capturas": [1, 0, 0, 0, 2, 1]})
-    obtained = set_up_ramsey_time_series(data_2)
+    obtained = set_up_ramsey_time_series(singular_data)
 
     expected = pd.DataFrame(
         {
