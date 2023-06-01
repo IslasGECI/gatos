145ca55475269ecf51132391ac1bfd9a4f481655
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-01 18:54:56 +0000

    ✅ Pass tests

--- gatos/gatos_cli.py
+++ gatos/gatos_cli.py
@@ -1,4 +1,7 @@
-from gatos.fit_ramsey_time_series import add_slopes_to_effort_capture_data
+from gatos.fit_ramsey_time_series import (
+    add_slopes_to_effort_capture_data,
+    add_probability_to_effort_capture_data,
+)
 from gatos.get_capture_and_effort_by_zone import (
     calculate_yearly_cumulative_cpue,
     get_cummulative_effort_and_captures_by_year,
@@ -38,6 +41,16 @@ def write_yearly_cummulative_effort_and_captures(
         weekly_effort_and_capture, years_in_data
     )
     cummulatie_effort_and_capture.to_csv(output_path, index=False)
+
+
+@app.command()
+def write_effort_and_captures_with_probability(
+    input_path: str = typer.Option("", help="Input file path"),
+    output_path: str = typer.Option("", help="Output file path"),
+):
+    effort_capture_data = pd.read_csv(input_path)
+    effort_captures_with_slopes = add_probability_to_effort_capture_data(effort_capture_data)
+    effort_captures_with_slopes.to_csv(output_path, index=False)
 
 
 @app.command()
