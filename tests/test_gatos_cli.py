e415f3bae3354967584d94f1a79effbe3890bdc9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-01 18:39:42 +0000

    🛑🧪 Fail tests

--- tests/test_gatos_cli.py
+++ tests/test_gatos_cli.py
@@ -1,5 +1,6 @@
 from gatos import (
     write_effort_and_captures_by_zone_for_year,
+    write_effort_and_captures_with_probability,
     write_effort_and_captures_with_slopes,
     write_yearly_cummulative_effort_and_captures,
 )
@@ -9,7 +10,7 @@ import pandas as pd
 import os
 import hashlib
 
-
+np.reshape
 input_path = "tests/data/esfuerzo_capturas_semanales_iso8601.csv"
 
 
@@ -20,6 +21,34 @@ def test_write_effort_and_captures_by_zone():
     year = "2022"
     write_effort_and_captures_by_zone_for_year(input_path, output_path, year)
     assert os.path.exists(output_path)
+
+
+def test_write_effort_and_capture_with_probability():
+    output_path = "tests/data/probability_time_series.csv"
+    monthly_path = "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    write_effort_and_captures_with_probability(monthly_path, output_path)
+    assert os.path.exists(output_path)
+    obtained = pd.read_csv(output_path)
+
+    obtained_slopes = obtained.slope
+    expected_slopes = pd.Series(
+        [
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            np.nan,
+            4.739e-6,
+            -1.117e-5,
+            -9.646e-6,
+            -5.082e-5,
+            -2.781e-6,
+        ],
+        name="slope",
+    )
+    pd.testing.assert_series_equal(obtained_slopes, expected_slopes)
 
 
 def test_write_effort_and_capture_with_slopes():
