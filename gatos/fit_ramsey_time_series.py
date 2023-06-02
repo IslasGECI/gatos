98545a76948247bc2be2a37146d52b40281b7339
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-02 18:00:09 +0000

    ♻️  Refactor

--- gatos/fit_ramsey_time_series.py
+++ gatos/fit_ramsey_time_series.py
@@ -14,6 +14,10 @@ def add_probability_to_effort_capture_data(data):
 
 
 def set_up_effort_capture_data(data):
+    return xxset_up_effort_capture_data(data)
+
+
+def xxset_up_effort_capture_data(data):
     data_copy = data.copy()
     data_copy = remove_consecutive_non_captures(data_copy)
     add_empty_column(data_copy, column_name="prob")
