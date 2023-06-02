b8904d6cc9a71aa65aaf3f92f5ca11db355edba6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-02 18:02:14 +0000

    ♻️  Refactor

--- gatos/fit_ramsey_time_series.py
+++ gatos/fit_ramsey_time_series.py
@@ -7,13 +7,13 @@ from eradication_data_requirements import fit_ramsey_plot
 
 
 def add_probability_to_effort_capture_data(data):
-    data_copy = xxset_up_effort_capture_data(data, "prob")
+    data_copy = set_up_effort_capture_data(data, "prob")
     probs_status = get_status_probs(data_copy)
     data_copy.loc[5:, "prob"] = probs_status
     return data_copy
 
 
-def xxset_up_effort_capture_data(data, column_name):
+def set_up_effort_capture_data(data, column_name):
     data_copy = data.copy()
     data_copy = remove_consecutive_non_captures(data_copy)
     add_empty_column(data_copy, column_name)
