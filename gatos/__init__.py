9b7925dbd0fa04b3dc109581ec767cb11879940c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-24 21:33:28 +0000

    ✅ Pass tests

--- gatos/__init__.py
+++ gatos/__init__.py
@@ -8,4 +8,5 @@ from .fit_ramsey_time_series import *  # noqa
 from .gatos_cli import *  # noqa
 from .generaTamanioPoblacionGatos import *  # noqa
 from .get_capture_and_effort_by_zone import *  # noqa
+from .plot_progress_probability import *  # noqa
 from .remove_consecutive_non_captures import *  # noqa
---
+++ gatos/plot_progress_probability.py
@@ -0,0 +1,2 @@
+def plot_progress_probability(data):
+    pass
