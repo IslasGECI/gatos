a6d5abb12ed854c2373b155541296f6ffa378ae9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-12 20:24:21 +0000

    ♻️  Refactor

--- tests/test_fit_ramsey_time_series.py
+++ tests/test_fit_ramsey_time_series.py
@@ -19,14 +19,14 @@ data = pd.DataFrame({"Esfuerzo": [1, 2, 3, 4, 5, 6], "Capturas": [1, 1, 1, 1, 1,
 
 
 def test_add_probability_to_effort_capture_data():
-    obtained = add_probability_to_effort_capture_data(data)
+    obtained = get_status_probs(data)
     contains_slope_column = "prob" in obtained.columns
     assert contains_slope_column
 
     effort_and_capture_data = pd.read_csv(
         "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
     )
-    obtained = add_probability_to_effort_capture_data(effort_and_capture_data)
+    obtained = get_status_probs(effort_and_capture_data)
     obtained_probs = obtained.prob.iloc[6:]
     is_positive = obtained_probs >= 0
     assert is_positive.all()
