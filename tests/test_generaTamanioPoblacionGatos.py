4ecbd9d0bc57851e41bc760949cf16702590ba97
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-15 22:43:01 +0000

    🙈 pass check

--- tests/test_generaTamanioPoblacionGatos.py
+++ tests/test_generaTamanioPoblacionGatos.py
@@ -1,9 +1,7 @@
 from gatos import run_population_estimator
 
-import hashlib
 import subprocess
 import re
-import pandas as pd
 import json
 import numpy as np
 import os
@@ -30,17 +28,17 @@ def tests_run_population_estimator():
     assert os.path.exists(loo_figure)
     assert os.path.exists(predictive_figure)
 
-    obtained_posterior = pd.read_csv(posterior_path)
-    expected_posterior = pd.read_csv("tests/data/distribucion_posterior_reference.csv")
+    # obtained_posterior = pd.read_csv(posterior_path)
+    # expected_posterior = pd.read_csv("tests/data/distribucion_posterior_reference.csv")
     # pd.testing.assert_frame_equal(obtained_posterior, expected_posterior)
 
-    loo_path = "reports/non-tabular/loo_results.json"
-    obtained_loo_hash = hashlib.md5(open(loo_path, "rb").read()).hexdigest()
-    expected_loo_hash = "88b5f9b02123a9466803b1e2531aaded"
+    # loo_path = "reports/non-tabular/loo_results.json"
+    # obtained_loo_hash = hashlib.md5(open(loo_path, "rb").read()).hexdigest()
+    # expected_loo_hash = "88b5f9b02123a9466803b1e2531aaded"
     # assert obtained_loo_hash == expected_loo_hash
 
-    obtained_waic_path = "reports/non-tabular/waic_results.json"
-    expected_waic_path = "tests/data/waic_results.json"
+    # obtained_waic_path = "reports/non-tabular/waic_results.json"
+    # expected_waic_path = "tests/data/waic_results.json"
     # assert_dict_equal_from_path(obtained_waic_path, expected_waic_path)
 
     posterior_path = "tests/data/distribucion_posterior_without_datapackage.csv"
@@ -57,11 +55,11 @@ def tests_run_population_estimator():
 
     assert os.path.exists(predictive_figure)
 
-    obtained_posterior = pd.read_csv(posterior_path)
-    expected_posterior = pd.read_csv("tests/data/distribucion_posterior_reference.csv")
+    # obtained_posterior = pd.read_csv(posterior_path)
+    # expected_posterior = pd.read_csv("tests/data/distribucion_posterior_reference.csv")
     # pd.testing.assert_frame_equal(obtained_posterior, expected_posterior)
 
-    obtained_loo_hash = hashlib.md5(open(loo_path, "rb").read()).hexdigest()
+    # obtained_loo_hash = hashlib.md5(open(loo_path, "rb").read()).hexdigest()
     # assert obtained_loo_hash == expected_loo_hash
 
     # assert_dict_equal_from_path(obtained_waic_path, expected_waic_path)
