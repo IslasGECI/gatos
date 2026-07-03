206a7cd5d047d97750493fc6674cef90f4ef3b8a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-03 20:19:50 +0000

    ✅ Pass tests

--- gatos/compute_semestral_effort_and_captures.py
+++ gatos/compute_semestral_effort_and_captures.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 def compute_semestral_effort_and_captures(effort_and_captures_df):
     effort_and_captures_df.index = pd.to_datetime(effort_and_captures_df.Fecha)
-    effort_and_captures_df["semestral"] = (
-        effort_and_captures_df.index.year.astype(str)
-        + "-S"
-        + np.where(effort_and_captures_df.index.month <= 6, "1", "2")
+    resolution = 6
+    period_index = (effort_and_captures_df.index.month - 1) // resolution
+    effort_and_captures_df["period"] = (
+        effort_and_captures_df.index.year.astype(str) + "-P" + (period_index + 1).astype(str)
     )
-    return effort_and_captures_df.groupby("semestral").agg({"Esfuerzo": "sum", "Capturas": "sum"})
+    return effort_and_captures_df.groupby("period").agg({"Esfuerzo": "sum", "Capturas": "sum"})
