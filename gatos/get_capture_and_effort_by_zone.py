9bafbe22097058c505f9c69b45905021768309d6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-03 17:38:14 +0000

    ♻️ Refactor

--- gatos/get_capture_and_effort_by_zone.py
+++ gatos/get_capture_and_effort_by_zone.py
@@ -52,6 +52,11 @@ def calculate_yearly_cumulative_cpue(data, years_in_data):
 
 
 def calculate_yearly_cumulative_effort_and_captures(datos_gatos_socorro, years_in_data):
+    return xxcalculate_yearly_cumulative_effort_and_captures(datos_gatos_socorro)
+
+
+def xxcalculate_yearly_cumulative_effort_and_captures(datos_gatos_socorro):
+    years_in_data = years_from_data(datos_gatos_socorro)
     masks = [datos_gatos_socorro["Fecha"].str.contains(i) for i in years_in_data]
     esfuerzo_acumulado_anual = [
         calculate_cumulative_effort_by_mask(datos_gatos_socorro, mask) for mask in masks
