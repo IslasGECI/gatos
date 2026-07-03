409bdeabafeada303477d38518432ebadd6f0d56
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-03 18:49:08 +0000

    🛑🧪 Fail tests

--- tests/test_compute_semestral_effort_and_captures.py
+++ tests/test_compute_semestral_effort_and_captures.py
@@ -5,3 +5,5 @@ import pandas as pd
 def test_compute_semestral_effort_and_captures():
     effort_and_captures_df = pd.read_csv("tests/data/esfuerzo_capturas_semanales_iso8601.csv")
     obtained = compute_semestral_effort_and_captures(effort_and_captures_df)
+    expected_rows = 2
+    assert len(obtained) == expected_rows, f"Expected {expected_rows} rows, but got {len(obtained)}"
