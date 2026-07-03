9a4c70ea4603b7905d113407ab4948d3010ebbe9
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-03 20:22:25 +0000

    ♻️ Refactor

--- gatos/compute_semestral_effort_and_captures.py
+++ gatos/compute_semestral_effort_and_captures.py
@@ -5,6 +5,10 @@ import pandas as pd
 def compute_semestral_effort_and_captures(effort_and_captures_df):
     effort_and_captures_df.index = pd.to_datetime(effort_and_captures_df.Fecha)
     resolution = 6
+    return compute_effort_and_captures_by_resolution(effort_and_captures_df, resolution)
+
+
+def compute_effort_and_captures_by_resolution(effort_and_captures_df, resolution):
     period_index = (effort_and_captures_df.index.month - 1) // resolution
     effort_and_captures_df["period"] = (
         effort_and_captures_df.index.year.astype(str) + "-P" + (period_index + 1).astype(str)
