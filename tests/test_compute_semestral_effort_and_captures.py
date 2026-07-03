9333cd75cca991d8c09f3d4e66e1c042311692cc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-03 19:20:49 +0000

    🛑🧪 Fail tests

--- tests/test_compute_semestral_effort_and_captures.py
+++ tests/test_compute_semestral_effort_and_captures.py
@@ -7,3 +7,5 @@ def test_compute_semestral_effort_and_captures():
     obtained = compute_semestral_effort_and_captures(effort_and_captures_df)
     expected_rows = 2
     assert len(obtained) == expected_rows, f"Expected {expected_rows} rows, but got {len(obtained)}"
+    expected_columns = ["Esfuerzo", "Capturas"]
+    assert obtained.columns == expected_columns
